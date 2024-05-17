c813ed0c14deb6424b1703170bc3645807c34633
VanChien411 <phuthienchien3@gmail.com>
2024-05-18 00:07:00 +0700

    sf

--- websocket.py
+++ websocket.py
@@ -28,25 +28,31 @@ def predict():
 clients = {}
 
 async def register(websocket, user_id):
+    # Lưu kết nối của người dùng dựa trên user_id
     clients[user_id] = websocket
-
 async def unregister(user_id):
+    # Xóa kết nối của người dùng dựa trên user_id
     if user_id in clients:
         del clients[user_id]
-
+            
 async def echo(websocket, path):
     async for data in websocket:
-        data = json.loads(data)  # Use json.loads for security
+        data = eval(data)  # Convert string to dictionary (Insecure, use safer methods in production)
         user_id = data.get("user_id")
         friend_id = data.get("friend_id")
+        
         message = data.get("message")
+        print(f"Received message from user 2 {user_id}: {message} :{friend_id}")
         await register(websocket, user_id)
+         # Gửi tin nhắn đến tất cả các client
         for client_user_id, client_socket in clients.items():
             if client_user_id != user_id:
-                data = {"user_id": user_id, "message": message, "friend_id": friend_id}
-                json_data = json.dumps(data)
-                await client_socket.send(json_data)
-        await unregister(user_id)
+                data = {"user_id": user_id, "message": message, "friend_id":friend_id}
+                json_data = json.dumps(data)  # Chuyển đổi từ điển thành chuỗi JSON
+                await client_socket.send(json_data)  # Gửi chuỗi JSON
+     
+    # Sau khi kết thúc kết nối, gọi hàm unregister để xóa kết nối của người dùng
+    await unregister(user_id)
 
 async def main():
     # Run Flask app in a separate thread
