e2df9d20f7c27154d98c0f0f8983e6ea7bf32e1e
VanChien411 <phuthienchien3@gmail.com>
2024-06-05 08:29:20 +0700

    d

--- websocket.py
+++ websocket.py
@@ -1,6 +1,5 @@
-from flask import Flask, request, jsonify
+from flask import Flask, jsonify
 from flask_cors import CORS
-from gradio_client import Client
 import asyncio
 import websockets
 import json
@@ -8,21 +7,6 @@ import os
 
 app = Flask(__name__)
 CORS(app)
-# client = Client("ShynBui/Vector_db_v3")
-
-# Flask app routes and functions
-# @app.route("/predict", methods=["POST"])
-# def predict():
-#     data = request.json
-#     result = client.predict(
-#         quote=data['quote'],
-#         history=data['history'],
-#         api_name="/predict"
-#     )
-#     if result:
-#         return jsonify(result), 200
-#     else:
-#         return jsonify({"error": "Failed to connect to database"}), 500
 
 # WebSocket server functions
 clients = {}
@@ -30,32 +14,30 @@ clients = {}
 async def register(websocket, user_id):
     # Lưu kết nối của người dùng dựa trên user_id
     clients[user_id] = websocket
+
 async def unregister(user_id):
     # Xóa kết nối của người dùng dựa trên user_id
     if user_id in clients:
         del clients[user_id]
-            
+
 async def echo(websocket, path):
-    async for data in websocket:
-        data = eval(data)  # Convert string to dictionary (Insecure, use safer methods in production)
+    async for message in websocket:
+        data = json.loads(message)  # Sử dụng json.loads để chuyển đổi chuỗi JSON thành từ điển
         user_id = data.get("user_id")
         friend_id = data.get("friend_id")
-        
-        message = data.get("message")
-        print(f"Received message from user 2 {user_id}: {message} :{friend_id}")
+
+        print(f"Received message from user {user_id}: {data['message']} to friend {friend_id}")
         await register(websocket, user_id)
-         # Gửi tin nhắn đến tất cả các client
-        for client_user_id, client_socket in clients.items():
-            if client_user_id != user_id:
-                data = {"user_id": user_id, "message": message, "friend_id":friend_id}
-                json_data = json.dumps(data)  # Chuyển đổi từ điển thành chuỗi JSON
-                await client_socket.send(json_data)  # Gửi chuỗi JSON
-     
+        # Gửi tin nhắn đến bạn bè của người dùng
+        if friend_id in clients:
+            await clients[friend_id].send(json.dumps(data))  # Gửi chuỗi JSON
+
     # Sau khi kết thúc kết nối, gọi hàm unregister để xóa kết nối của người dùng
     await unregister(user_id)
 
 async def main():
-    async with websockets.serve(echo,host='', port=int(os.environ["PORT"])):
+    port = int(os.environ.get("PORT", "8000"))  # Đặt giá trị mặc định cho PORT nếu không được định nghĩa trong biến môi trường
+    async with websockets.serve(echo, host='', port=port):
         await asyncio.Future()  # Run forever
 
 if __name__ == "__main__":
